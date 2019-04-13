59c968c054a737b826a75801f7c9008eb54696e0
thomasgainant <thomasgainant@gmail.com>
2019-04-13 13:35:24 +0200

    Base code

---
+++ Assets/Entity.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		this.init();
+	}
+
+	public virtual void init(){
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		this.update();
+	}
+
+	protected virtual void update(){
+
+	}
+}
---
+++ Assets/Tile.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : Entity {
+
+	// Use this for initialization
+	public override void init(){
+
+	}
+	
+	// Update is called once per frame
+	protected override void update(){
+		
+	}
+}
