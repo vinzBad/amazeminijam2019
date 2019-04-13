dd23c76b22ff3d9232526d0ef629725e4ad5f58c
Vinzenz Sinapius <vinzenz.sinapius@gmail.com>
2019-04-13 15:34:38 +0200

    Add labels to debug draw

--- Assets/Map.cs
+++ Assets/Map.cs
@@ -46,4 +46,27 @@ public class Map : Entity {
 			Debug.DrawLine(start, end);
 		}
 	}
+
+	void OnGUI()
+    {
+        debugDrawMapLabels();
+    }
+
+	void debugDrawMapLabels() {
+		GUI.Label(new Rect(10, 10, 100, 20), "Hello World!");
+		var worldPosition = new Vector3();
+		var screenPosition = new Vector3();
+		var labelDimensions = mainCam.WorldToScreenPoint(new Vector3(Level.TILESIZE, Level.TILESIZE, Level.TILESIZE));
+
+		// always skip the first line for better looks
+		for(int rowIndex = 0; rowIndex < Level.MAPHEIGHT; rowIndex++) {
+			for(int colIndex = 0; colIndex < Level.MAPWIDTH; colIndex++) {
+				worldPosition = new Vector3(colIndex * Level.TILESIZE, rowIndex * Level.TILESIZE, 0);
+				screenPosition = mainCam.WorldToScreenPoint(worldPosition);
+				
+				GUI.Label(new Rect(screenPosition.x, screenPosition.y, labelDimensions.x, labelDimensions.y), this.content[rowIndex, colIndex].State.ToString());
+			}
+		}
+		
+	}
 }
